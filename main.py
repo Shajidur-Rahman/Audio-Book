3b7e72b5ec59831acfa53f653277ab53db2c5a8c
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-06-30 18:13:00 +0600

    pdfs are added

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print(4234)
+import pyttsx3
+import PyPDF2
+
+book = open('my.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+speaker = pyttsx3.init()
+speaker.say("Hello World! I am Python")
+speaker.runAndWait()  # sudo apt install libespeak1
+
+# 8:39
