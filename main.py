54b6c9f3fd887ff9257d2ebad3bc0be6138238db
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-06-30 18:55:40 +0600

    about to finish

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pyttsx3
 import PyPDF2
 
-book = open('my.pdf', 'rb')
+
+book = open('pdfs/my.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 print(pages)
 speaker = pyttsx3.init()
-speaker.say("Hello World! I am Python")
-speaker.runAndWait()  # sudo apt install libespeak1
 
-# 8:39
+
+for i in range(7, pages):
+    page = pdfReader.getPage(i)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()  # sudo apt install libespeak1
